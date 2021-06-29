5a67d7aafd70a8f82541171c2cc1e37d85ffa7bd
andreiolar-amdaris <andrei.olar@amdaris.com>
2021-06-29 12:44:31 +0300

    Created container deploy script

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -5,7 +5,7 @@ import MarketingApp from "./components/MarketingApp";
 const App = () => {
   return (
     <div>
-      <h1>Containe</h1>
+      <h1>Container</h1>
       <hr />
       <MarketingApp />
     </div>
