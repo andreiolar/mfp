3b68bd3227582b5a86729f38ae1146ea0fb3a754
andreiolar-amdaris <andrei.olar@amdaris.com>
2021-06-30 08:33:40 +0300

    Added public path config

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -10,6 +10,7 @@ const prodConfig = {
   mode: "production",
   output: {
     filename: "[name].[contenthash].js",
+    publicPath: "/container/latest/",
   },
   plugins: [
     new ModuleFederationPlugin({
