673123050f28cc6086dab513e0384112c2d8bec6
andreiolar-amdaris <andrei.olar@amdaris.com>
2021-07-01 12:31:35 +0300

    Added marketing deployment

--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -8,6 +8,7 @@ const prodConfig = {
   mode: "production",
   output: {
     filename: "[name].[contenthash].js",
+    publicPath: "/marketing/latest/",
   },
   plugins: [
     new ModuleFederationPlugin({
