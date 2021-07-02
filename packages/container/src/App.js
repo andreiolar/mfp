97c8de79b8ee1821cade68e6992afff2f60f7483
andreiolar-amdaris <andrei.olar@amdaris.com>
2021-07-02 08:48:35 +0300

    Fixing class names collision

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
-import { StylesProvider } from "@material-ui/core/styles";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
 
 import MarketingApp from "./components/MarketingApp";
 import Header from "./components/Header";
